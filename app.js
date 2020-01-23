7e3879dbc5dfadfa2bb6d9e63e535489b7a1b3eb
Talon-Dechert <tdechert@live.com>
2020-01-22 20:55:21 -0800

    Added functionality to upload images to server. Unfortunately file ext's are case sensitive still

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authentication = require('./authenticate');
 var config = require('./config');
+const uploadRouter = require('./routes/uploadRouter');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +19,6 @@ var leaderRouter = require('./routes/leaderRouter');
 
 const Dishes = require('./models/dishes');
 
-var app = express();
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
@@ -28,6 +28,20 @@ connect.then(
   },
   err => console.log(err)
 );
+
+var app = express();
+
+//Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(
+      307,
+      'https://' + req.hostname + ':' + app.get('secPort') + req.url
+    );
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -48,6 +62,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

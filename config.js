aeb9fa7af090a001e0c8deca908073a910f52c2b
Talon-Dechert <tdechert@live.com>
2020-01-22 22:55:59 -0800

    Added facebook authentication via passport-facebook-token

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
-  mongoUrl: 'mongodb://localhost:27017/conFusion'
+  mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '477502526301682',
+    clientSecret: 'fa811f360e820fd1ed2131c7c93f6629'
+  }
 };
